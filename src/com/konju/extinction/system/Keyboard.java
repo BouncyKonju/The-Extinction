6f97a1fe173af5e9dfb731c8243130dc2d6684f5
BouncyKonju <konjuart@gmail.com>
2016-07-23 18:44:35 +0200

    Keyboard input implemented

---
+++ src/com/konju/extinction/system/Keyboard.java
@@ -0,0 +1,28 @@
+package com.konju.extinction.system;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener {
+	
+	public static final int K_W = KeyEvent.VK_W;
+	public static final int K_A = KeyEvent.VK_A;
+	public static final int K_S = KeyEvent.VK_S;
+	public static final int K_D = KeyEvent.VK_D;
+	
+	public static boolean[] keys = new boolean[1024];
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		keys[e.getKeyCode()] = true;
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		keys[e.getKeyCode()] = false;
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {}
+
+}
